3b0c42e59c95fe11f72f779d5b5441b8e6978d19
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2023-05-20 03:22:51 +0600

    post data on mongogodb

--- index.js
+++ index.js
@@ -12,13 +12,10 @@ const port = process.env.PORT || 2000;
 app.use(cors())
 app.use(express.json())
 
-
-console.log(process.env.GAME_USER);
-console.log(process.env.GAME_PASS);
-
 app.get("/", (req, res) => {
     res.send("GameZone Toy Shop is running")
 })
+
 
 
 const uri = `mongodb+srv://${process.env.GAME_user}:${process.env.GAME_pass}@cluster0.yaanftr.mongodb.net/?retryWrites=true&w=majority`;
@@ -34,8 +31,18 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+        const gamezoneCollection = client.db("toyShop").collection("allToys");
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        //post single data to mongodb
+
+        app.post("/toyshops", async (req, res) => {
+            const shopping = req.body;
+            const result = await gamezoneCollection.insertOne(shopping)
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
