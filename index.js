fe37dd3d5a2a86ae17ca24eac80c33873a4a0200
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2023-05-20 03:34:15 +0600

    getting data from mongodb

--- index.js
+++ index.js
@@ -43,6 +43,14 @@ async function run() {
             res.send(result)
         })
 
+        //get data from mongodb
+
+        app.get("/toyshops", async (req, res) => {
+            const cursor = gamezoneCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
